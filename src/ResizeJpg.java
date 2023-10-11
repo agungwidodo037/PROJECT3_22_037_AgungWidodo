e504f3ea22db3ebaef423e7befbe93334df26168
agungwidodo037 <146101856+agungwidodo037@users.noreply.github.com>
2023-10-11 19:19:58 +0700

    Add files via upload

---
+++ src/ResizeJpg.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JLabel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asus
+ */
+class ResizeJpg {
+
+    ResizeJpg(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setImage(String d19Jurusjpg, JLabel lbDepan) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
